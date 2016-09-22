c94f4cca9bd11f63751f93487c14d34793c4215f
Nico Thiebes <nico.thiebes@gmail.com>
2016-09-22 19:26:18 +0200

    header wip

--- _source/01_atoms/link/Link.jsx
+++ _source/01_atoms/link/Link.jsx
@@ -10,11 +10,12 @@ export default class Link extends React.Component {
     render() {
         const PROPS = this.props;
         const CLASS = 'a-link ' + PROPS.className;
+        const ICON = PROPS.icon ? <Icon icon={ PROPS.icon } /> : '';
         const TEXT = PROPS.text ? PROPS.text : '';
 
         return (
             <a className={ CLASS } href={ PROPS.href } title={ PROPS.title }>
-                { TEXT }
+                { ICON }{ TEXT }
             </a>
         );
     }
