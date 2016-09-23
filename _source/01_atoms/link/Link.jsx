33b7e212f55649bab5e6c0e0e0b753fcf84b31cd
Nico Thiebes <nico.thiebes@gmail.com>
2016-09-23 12:32:55 +0200

    Button styles (#3)

--- _source/01_atoms/link/Link.jsx
+++ _source/01_atoms/link/Link.jsx
@@ -10,7 +10,7 @@ export default class Link extends React.Component {
     render() {
         const PROPS = this.props;
         const CLASS = 'a-link ' + PROPS.className;
-        const ICON = PROPS.icon ? <Icon icon={ PROPS.icon } className="a-icon--light" /> : '';
+        const ICON = PROPS.icon ? <Icon icon={ PROPS.icon } /> : '';
         const TEXT = PROPS.text ? PROPS.text : '';
 
         return (
--- _source/03_organisms/header/Header.jsx
+++ _source/03_organisms/header/Header.jsx
@@ -14,11 +14,21 @@ export default class Header extends React.Component {
         return (
             <header className="o-header">
                 <Icon icon="menu" className="o-header__menu-main-icon a-icon--light" />
+                <label className="o-header__label o-header__label--light">Menu</label>
                 <MenuMain />
-                <Link className="o-header__logo" href="/" title="Home" icon="heart" />
-                <MenuAccount />
-                <Icon icon="more" className="o-header__menu-account-icon a-icon--light" />
+
+                <Link className="o-header__logo o-header__logo--small a-link--light" href="/" title="Home" icon="heart" />
+                <Link className="o-header__logo o-header__logo--large a-link--light" href="/" title="Home" />
+
+                <Icon icon="search" className="o-header__menu-account-icon a-icon--light" />
+                <Icon icon="add" className="o-header__menu-account-icon a-icon--light" />
+                <Icon icon="dashboard" className="o-header__menu-account-icon a-icon--light" />
             </header>
         );
     }
 }
+
+/**
+<MenuAccount />
+<Icon icon="more" className="o-header__menu-account-icon a-icon--light" />
+ */
