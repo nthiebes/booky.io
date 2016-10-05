891e2b4f61c4ffb8e17725b68e215a81dda5b119
Nico Thiebes <nico.thiebes@gmail.com>
2016-10-05 16:54:51 +0200

    toolbar specs

---
+++ _source/02_molecules/search/search.spec.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Search from './Search';
+
+describe('<Search />', function() {
+
+    it('Should have a stupid test', function() {
+
+    });
+});
--- _source/03_organisms/toolbar/toolbar.spec.jsx
+++ _source/03_organisms/toolbar/toolbar.spec.jsx
@@ -2,23 +2,80 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Toolbar from './Toolbar';
+import Button from '../../01_atoms/button/Button.jsx';
 import Search from '../../02_molecules/search/Search.jsx';
 import Icon from '../../01_atoms/icon/Icon.jsx';
-import Button from '../../01_atoms/button/Button.jsx';
 
 describe('<Toolbar />', function() {
 
-    it('Should have a stupid test', function() {
-        const toolbar = shallow(<Toolbar searchOpen={ false } />);
-        
-        expect(toolbar.find('.o-toolbar').length).toBe(1);
+    var toolbar,
+        callback,
+        getComponent = function(props) {
+            return <Toolbar {...props} />;
+        };
 
-        expect(toolbar.contains(
-            <Button className="o-toolbar__button a-button--primary" text="New " buzzword="category" />)
-        ).toEqual(true);
+    beforeEach(function() {
+        callback = jest.fn();
+    });
 
-        expect(toolbar.contains(
-            <Search className="" open={ false } />)
-        ).toEqual(true);
+    describe('when rendered open', function() {
+
+        beforeEach(function() {
+            toolbar = shallow(getComponent({
+                searchOpen: true
+            }));
+        });
+
+        it('should have the correct class', function() {
+            expect(toolbar.find('.o-toolbar').length).toBe(1);
+            expect(toolbar.find('.o-toolbar--open').length).toBe(1);
+        });
+
+        it('should include a search bar', function() {
+            expect(toolbar.contains(
+                <Search className="m-search--open" open={ true } />
+            )).toEqual(true);
+        });
+    });
+
+    describe('when rendered closed', function() {
+
+        beforeEach(function() {
+            toolbar = shallow(getComponent({
+                searchOpen: false
+            }));
+        });
+
+        it('should have the correct class', function() {
+            expect(toolbar.find('.o-toolbar').length).toBe(1);
+            expect(toolbar.find('.o-toolbar--open').length).toBe(0);
+        });
+
+        it('should include a search bar', function() {
+            expect(toolbar.contains(
+                <Search className="" open={ false } />
+            )).toEqual(true);
+        });
+    });
+
+    describe('should always', function() {
+
+        beforeEach(function() {
+            toolbar = shallow(getComponent({
+                onSearchClick: callback
+            }));
+        });
+
+        it('include a button for new categories', function() {
+            expect(toolbar.contains(
+                <Button className="o-toolbar__button a-button--primary" text="New " buzzword="category" />
+            )).toEqual(true);
+        });
+
+        it('include an icon', function() {
+            expect(toolbar.contains(
+                <Icon icon="search" className="o-toolbar__icon a-icon--dark" onClick={ callback } />
+            )).toEqual(true);
+        });
     });
 });
