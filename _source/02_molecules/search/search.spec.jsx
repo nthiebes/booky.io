4cb0358e4bb04490d22f352ca7b6628dc1c9dd5d
Nico Thiebes <nico.thiebes@gmail.com>
2016-10-05 16:55:01 +0200

    base search specs

--- _source/02_molecules/search/search.spec.jsx
+++ _source/02_molecules/search/search.spec.jsx
@@ -6,6 +6,8 @@ import Search from './Search';
 describe('<Search />', function() {
 
     it('Should have a stupid test', function() {
+        const SEARCH = shallow(<Search />);
 
+        expect(SEARCH.find('.m-search').length).toBe(1);
     });
 });
