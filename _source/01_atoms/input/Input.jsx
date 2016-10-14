132868611cf4f0bf2862a7e133764ff836e35d0d
Nico Thiebes <nico.thiebes@gmail.com>
2016-10-15 00:17:47 +0200

    input component

--- _source/01_atoms/input/Input.jsx
+++ _source/01_atoms/input/Input.jsx
@@ -1,34 +1,49 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import { findDOMNode } from 'react-dom';
 
-export default class Input extends React.Component {
+/**
+ * React component
+ * @class 01_atoms/input/Input
+ *
+ * @prop {string}  [className]   Additional class name
+ * @prop {boolean} [focus]       Autofocus input field
+ * @prop {string}  [placeholder] Input placeholder
+ * @prop {string}  [type]        Input type
+ */
+export default class Input extends Component {
     componentDidUpdate() {
         if (this.props.focus) {
-            findDOMNode(this.refs.nameInput).focus(); 
+            findDOMNode(this.refs.inputField).focus();
         }
     }
 
     render() {
         const PROPS = this.props;
+        const CLASS = 'a-input ' + PROPS.className;
         const INPUT_PROPS = {
             'className': 'a-input__field',
-            'placeholder': PROPS.placeholder
+            'placeholder': PROPS.placeholder,
+            'type': PROPS.type
         };
 
         return (
-            <div className="a-input">
-                <input ref="nameInput" { ...INPUT_PROPS } />
+            <div className={ CLASS }>
+                <input ref="inputField" { ...INPUT_PROPS } />
             </div>
         );
     }
 }
 
 Input.propTypes = {
-    'focus': React.PropTypes.bool,
-    'placeholder': React.PropTypes.string
+    'className': PropTypes.string,
+    'focus': PropTypes.bool,
+    'placeholder': PropTypes.string,
+    'type': PropTypes.string
 };
 
 Input.defaultProps = {
+    'className': '',
     'focus': false,
-    'placeholder': ''
+    'placeholder': '',
+    'type': 'text'
 };
---
+++ _source/01_atoms/input/input.spec.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Input from './Input.jsx';
+
+describe('<Input />', function() {
+
+    let component;
+        
+    const getComponent = function(props = {}) {
+        return <Input { ...props } />;
+    };
+
+    describe('when initialized without optional parameters', function() {
+
+        beforeEach(function() {
+            component = shallow(getComponent({}));
+        });
+
+        it('should have the correct class', function() {
+            expect(component.find('div').hasClass('a-input')).toBe(true);
+        });
+
+        it('should include a input field', function() {
+            const inputField = component.find('input');
+            const labelProps = inputField.props();
+
+            expect(labelProps).toEqual({
+                'className': 'a-input__field',
+                'placeholder': '',
+                'type': 'text'
+            });
+
+            expect(inputField.node.ref).toBe('inputField');
+        });
+    });
+
+    describe('when initialized with optional parameters', function() {
+
+        beforeEach(function() {
+            component = shallow(getComponent({
+                'placeholder': 'Gscheider placeholder!',
+                'className': 'banana',
+                'focus': true,
+                'type': 'password'
+            }));
+        });
+
+        it('should have the correct class', function() {
+            expect(component.find('div').hasClass('a-input banana')).toBe(true);
+        });
+
+        it('should include a input field', function() {
+            const inputField = component.find('input');
+            const labelProps = inputField.props();
+
+            expect(labelProps).toEqual({
+                'className': 'a-input__field',
+                'placeholder': 'Gscheider placeholder!',
+                'type': 'password'
+            });
+
+            expect(inputField.node.ref).toBe('inputField');
+        });
+    });
+});
