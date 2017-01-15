a3b4830a505e95d2da671387b1cbf316d8715950
Nico Thiebes <nico.thiebes@gmail.com>
2017-01-15 23:47:47 +0100

    headline test

--- _source/04_pages/about/About.jsx
+++ _source/04_pages/about/About.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import HeaderContainer from '../../03_organisms/header/headerContainer';
 import SidebarContainer from '../../03_organisms/sidebar/sidebarContainer';
+import Headline from '../../01_atoms/headline/Headline';
 
 /**
  * React component
@@ -18,6 +19,13 @@ export default class AboutPage extends Component {
             <div>
                 <HeaderContainer />
                 <SidebarContainer />
+                <main>
+                    <Headline type={ 1 } text="Servus, wie geht's?" />
+                    <Headline type={ 1 } text="Servus, wie geht's?" />
+                    <Headline type={ 1 } text="Category こんにちはお元気で 3" />
+                    <Headline type={ 1 } text="مرحبا كيف حالك؟" />
+                    <Headline type={ 1 } text="Привет, как дела 3" />
+                </main>
             </div>
         );
     }
