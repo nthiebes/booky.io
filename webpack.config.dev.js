3bebbbddb3440daf2b44e096af9d421ba336beb0
Nico Thiebes <nico.thiebes@gmail.com>
2017-02-03 21:26:39 +0100

    renamed index to main

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,7 +13,7 @@ export default {
     // must be first entry to properly set public path
     './_source/webpack-public-path',
     'webpack-hot-middleware/client?reload=true',
-    './_source/index'
+    './_source/main'
   ],
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
