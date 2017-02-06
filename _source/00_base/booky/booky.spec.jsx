1e433081a3bdf3d945137923969614c309abd9e5
Nico Thiebes <nico.thiebes@gmail.com>
2017-02-06 22:10:04 +0100

    removed component from booky specs

--- _source/00_base/booky/booky.spec.jsx
+++ _source/00_base/booky/booky.spec.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Booky from './Booky.jsx';
-
 describe('<Booky />', function() {
 
     it('should have a stupid test', function() {
