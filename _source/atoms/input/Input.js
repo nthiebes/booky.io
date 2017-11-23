d09b5791889753a792e5e7c0a84cba775ac4696f
Nico Thiebes <nico.thiebes@xing.com>
2017-11-23 13:53:03 +0100

    Add colors to input

--- _source/atoms/input/Input.js
+++ _source/atoms/input/Input.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { findDOMNode } from 'react-dom';
+import classNames from 'classnames';
 
 export default class Input extends Component {
   componentDidUpdate() {
@@ -10,35 +11,37 @@ export default class Input extends Component {
   }
 
   render() {
-    const { className, placeholder, type, onBlur, onFocus } = this.props;
-    const INPUT_PROPS = {
-      'className': `input__field ${className}`,
-      'placeholder': placeholder,
-      'type': type,
-      'onBlur': onBlur ? onBlur : null,
-      'onFocus': onFocus ? onFocus : null
+    const { className, placeholder, type, onBlur, onFocus, color } = this.props;
+    const inputProps = {
+      className: classNames('input__field', className && className, color && `input__field--color-${color}`),
+      placeholder: placeholder,
+      type: type,
+      onBlur: onBlur ? onBlur : null,
+      onFocus: onFocus ? onFocus : null
     };
 
     return (
       <div className={ `input ${className}` }>
-        <input ref="inputField" { ...INPUT_PROPS } />
+        <input ref="inputField" { ...inputProps } />
       </div>
     );
   }
 }
 
 Input.propTypes = {
-  'className': PropTypes.string,
-  'focus': PropTypes.bool,
-  'onBlur': PropTypes.func,
-  'onFocus': PropTypes.func,
-  'placeholder': PropTypes.string,
-  'type': PropTypes.string
+  className: PropTypes.string,
+  focus: PropTypes.bool,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  color: PropTypes.string
 };
 
 Input.defaultProps = {
-  'className': '',
-  'focus': false,
-  'placeholder': '',
-  'type': 'text'
+  className: '',
+  focus: false,
+  placeholder: '',
+  type: 'text',
+  color: ''
 };
