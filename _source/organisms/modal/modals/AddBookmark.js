bdbee51225066b8d221649d7fbf4eb15a5d176d7
Nico Thiebes <nico.thiebes@xing.com>
2017-12-29 23:54:09 +0100

    Add bookmark modal

--- _source/organisms/modal/modals/AddBookmark.js
+++ _source/organisms/modal/modals/AddBookmark.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Base from '../Base';
 import Input from '../../../atoms/input';
 
 export default class AddBookmark extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onUrlChange = this.onUrlChange.bind(this);
+    this.state = {
+      name: '',
+      url: ''
+    };
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      name: '',
+      url: ''
+    });
+  }
+
+  onNameChange(value) {
+    this.setState({
+      name: value
+    });
+  }
+
+  onUrlChange(value) {
+    this.setState({
+      url: value
+    });
+  }
+
   render() {
+    const { onClose, onSave } = this.props;
+    const { name, url } = this.state;
+
     return (
-      <Base { ...this.props } headline="Add a bookmark">
-        <label className="modal__label" htmlFor="url">{ 'URL:' }</label>
-        <Input id="url" color="primary" required />
-        <label className="modal__label" htmlFor="name">{ 'Name:' }</label>
-        <Input id="name" color="primary" required />
+      <Base onClose={ onClose } onSave={ () => { onSave(this.state); } } headline="Add a bookmark">
+        <label className="modal__label" htmlFor="bookmark-url">{ 'URL:' }</label>
+        <Input id="bookmark-url" color="primary" value={ url } onChange={ this.onUrlChange } type="url" required />
+        <label className="modal__label" htmlFor="bookmark-name">{ 'Name:' }</label>
+        <Input id="bookmark-name" color="primary" value={ name } onChange={ this.onNameChange } required />
       </Base>
     );
   }
 }
+
+AddBookmark.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired
+};
