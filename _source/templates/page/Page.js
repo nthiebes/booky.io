2bfe7cd9d3fe3fe45f7195d78b0db545385d3bbc
Nico Thiebes <nico.thiebes@xing.com>
2018-09-15 00:28:18 +0200

    page loading animation

--- _source/templates/page/Page.js
+++ _source/templates/page/Page.js
@@ -9,24 +9,58 @@ import Modal from '../../organisms/modal';
 import Toolbar from '../../organisms/toolbar';
 
 export default class Page extends Component {
+  constructor(props) {
+    super(props);
+
+    this.show = this.show.bind(this);
+    this.state = {
+      show: false
+    };
+  }
+
+  componentDidMount() {
+    window.setTimeout(this.show, 100);
+  }
+
+  show() {
+    this.setState({
+      show: true
+    });
+  }
+
   render() {
     const { children, className, dashboards, compact, fullWidth, home, toolbar, blurContent } = this.props;
+    const { show } = this.state;
 
     return (
       <Fragment>
-        <Header className={ classNames(blurContent && 'page--blur') } dashboards={ dashboards } home={ home } />
-        { toolbar && <Toolbar className={ classNames(blurContent && 'page--blur') } /> }
-        <Sidebar className={ classNames(blurContent && 'page--blur') } dashboards={ dashboards } />
+        <Header
+          className={ classNames(blurContent && 'page--blur', 'page--hidden', show && 'page--show') }
+          dashboards={ dashboards }
+          home={ home }
+        />
+        { toolbar && (
+          <Toolbar className={ classNames(blurContent && 'page--blur', 'page--hidden', show && 'page--show') } />
+        ) }
+        <Sidebar
+          className={ classNames(blurContent && 'page--blur', 'page--hidden', show && 'page--show') }
+          dashboards={ dashboards }
+        />
         <main className={ classNames(
           'page',
           compact && 'page--compact',
           fullWidth && 'page--full-width',
           blurContent && 'page--blur',
+          'page--hidden',
+          show && 'page--show',
           className && className
         ) }>
           { children }
         </main>
-        <Footer className={ classNames(blurContent && 'page--blur') } home={ home } />
+        <Footer
+          className={ classNames(blurContent && 'page--blur', 'page--hidden', show && 'page--show') }
+          home={ home }
+        />
         <Modal />
       </Fragment>
     );
