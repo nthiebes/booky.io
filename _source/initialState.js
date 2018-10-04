c0f0790e4a0f39c3d3f5fcb071af6b593e4b77f0
Nico Thiebes <nico.thiebes@xing.com>
2018-10-05 00:06:02 +0200

    update initial state

--- _source/initialState.js
+++ _source/initialState.js
@@ -1,215 +1,26 @@
 export default {
   user: {
-    loggedIn: false
+    loggedIn: false,
+    navColor: '0',
+    newtab: true,
+    pinned: true,
+    dashboards: 'sidebar',
+    maxWidth: true,
+    preserveEditMode: true
   },
-  booky: {
-    headerColor: 0
-  },
-  'dashboards': {
-    'items': [{
-      'id': 567,
-      'name': 'Dashboard كيف ح вет, как 1',
-      categories: [{
-        'id': 0,
-        'name': 'Category 1 loooong naaaaame',
-        'position': 0
-      }, {
-        'id': 1,
-        'name': 'Category 2 loooong naaaaame',
-        'position': 2
-      }, {
-        'id': 2,
-        'name': 'Category 3 loooong naaaaame',
-        'position': 3
-      }]
-    }, {
-      'id': 678,
-      'name': 'Dashboard looooooooooooooooong 2',
-      categories: [{
-        'id': 3,
-        'name': 'Category 4 loooong naaaaame',
-        'position': 0
-      }, {
-        'id': 4,
-        'name': 'Category 5 loooong naaaaame',
-        'position': 1
-      }]
-    }, {
-      'id': 789,
-      'name': 'Dashboard こんにち 3',
-      categories: []
-    }],
-    active: 567,
+  dashboards: {
+    items: [{id: 1234, name: 'Meine Rezepte', categories: []}, {id: 90454, name: 'Deutsche Filme', categories: []}, {id: 789, name: 'Deutsche Filme', categories: []}, {id: 567, name: 'Deutsche Filme', categories: []}, {id: 45345, name: 'Deutsche Filme', categories: []}],
+    active: 1234,
     offset: 3,
-    open: true,
     sidebar: true
   },
-  'categories': [{
-    'id': 123,
-    'name': 'Category 1 loooong naaaaame',
-    'dashboard': 0,
-    'position': 0,
-    'color': 1,
-    'expanded': true,
-    bookmarks: [{
-      'id': 0,
-      'category': 0,
-      'name': 'booky.io 1',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 1,
-      'category': 0,
-      'name': 'Bookmark 2 veeeeeeery loooooong tiiiitle !!!!!!',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 2,
-      'category': 0,
-      'name': 'booky.io 3',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 3,
-      'category': 1,
-      'name': 'booky.io 4',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 4,
-      'category': 1,
-      'name': 'booky.io 5',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 5,
-      'category': 3,
-      'name': 'booky.io 6',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 6,
-      'category': 3,
-      'name': 'booky.io 7',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 7,
-      'category': 3,
-      'name': 'booky.io 8',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 8,
-      'category': 3,
-      'name': 'booky.io 9',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 9,
-      'category': 3,
-      'name': 'booky.io 10',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 10,
-      'category': 1,
-      'name': 'booky.io 11',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 11,
-      'category': 1,
-      'name': 'booky.io 12',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 12,
-      'category': 0,
-      'name': 'booky.io 13',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }, {
-      'id': 13,
-      'category': 0,
-      'name': 'booky.io 14',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }]
-  }, {
-    'id': 345,
-    'name': 'Category 2 مرحبا كيف حالك؟ asas!',
-    'dashboard': 0,
-    'position': 1,
-    'color': 2,
-    'expanded': true,
-    bookmarks: [{
-      'id': 14,
-      'category': 0,
-      'name': 'booky.io',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }]
-  }, {
-    'id': 45,
-    'name': 'Category 3 こんにちはお元気で 3',
-    'dashboard': 0,
-    'position': 2,
-    'color': 3,
-    'expanded': false,
-    bookmarks: []
-  }, {
-    'id': 76,
-    'name': 'Category 4 Привет, как дела 4',
-    'dashboard': 0,
-    'position': 3,
-    'color': 4,
-    'expanded': true,
-    bookmarks: [{
-      'id': 16,
-      'category': 0,
-      'name': 'booky.io',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }]
-  }, {
-    'id': 89,
-    'name': 'Category 5',
-    'dashboard': 2,
-    'position': 4,
-    'color': 0,
-    'expanded': false,
-    bookmarks: [{
-      'id': 17,
-      'category': 0,
-      'name': 'booky.io',
-      'url': 'https://booky.io',
-      'favicon': 'base64',
-      'position': 0
-    }]
-  }],
+  categories: [],
   sidebar: {
     open: false
   },
-  'toolbar': {
-    'sticky': true,
-    'currentlySticky': true
+  toolbar: {
+    sticky: true,
+    currentlySticky: true
   },
   modal: {
     modal: null,
