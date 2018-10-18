a20f84378d665eaed0eb6d7dcc04390740a75b1e
Samira Stein <samira.stein@xing.com>
2018-10-18 22:28:47 +0200

    Add dummy content to the help section

--- _source/pages/help/Help.js
+++ _source/pages/help/Help.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
+import classNames from 'classnames';
+import './Help.scss';
 
 import Page from '../../templates/page';
 import { H1 } from '../../atoms/headline';
+import P from '../../atoms/paragraph';
 import Section from '../../molecules/section';
 
 export default class Help extends Component {
   render() {
     return (
-      <Page>
+      <Page className="help">
         <Section>
           <H1>
             <FormattedMessage id="help.title" />
           </H1>
+          <P>
+            <FormattedMessage id="help.intro" />
+          </P>
+        </Section>
+        <Section>
+          <H1>
+            <FormattedMessage id="help.title" />
+          </H1>
+          <P>
+            <FormattedMessage id="help.intro" />
+          </P>
+        </Section>
+        <Section>
+          <H1>
+            <FormattedMessage id="help.title" />
+          </H1>
+          <P>
+            <FormattedMessage id="help.intro" />
+          </P>
+        </Section>
+        <Section>
+          <H1>
+            <FormattedMessage id="help.title" />
+          </H1>
+          <P>
+            <FormattedMessage id="help.intro" />
+          </P>
         </Section>
       </Page>
     );
