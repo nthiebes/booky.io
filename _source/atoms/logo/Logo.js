be5a4f6c422517df295e37a4fa2b63ebc8bc43e3
Samira Stein <samira.stein@xing.com>
2018-10-28 21:12:57 +0100

    First draft to use the logo with inverted color, might be refactored later :-)

--- _source/atoms/logo/Logo.js
+++ _source/atoms/logo/Logo.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import './Logo.scss';
+
 export default class Logo extends Component {
   render() {
-    const { className, src, alt, height, width } = this.props;
+    const { className, src, alt, height, width, colorInverted } = this.props;
     
     return (
-        <img src={ src } alt={ alt } height={ height } width={ width } className={ className } />
-      )
+      <img src={ src } alt={ alt } height={ height } width={ width } className={ classNames(colorInverted && 'logo--inverted', className && className )} />
+    )
   }
 }
 
@@ -16,6 +18,7 @@ Logo.propTypes = {
   className: PropTypes.string,
   src: PropTypes.string,
   alt: PropTypes.string,
+  colorInverted: PropTypes.boolean,
   height: PropTypes.string,
   width: PropTypes.string 
 }
@@ -23,5 +26,6 @@ Logo.propTypes = {
 Logo.defaultProps = {
   src: '../../_assets/logo-primary.png',
   alt: 'Logo',
-  height: '36'
+  height: '36',
+  colorInverted: false,
 }
