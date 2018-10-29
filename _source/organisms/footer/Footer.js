672eb9a5177ebe2af3aa035b4ad24878bb17f61b
Nico Thiebes <nico.thiebes@xing.com>
2018-10-29 09:28:18 +0100

    add xing share icon + update footer structure

--- _source/organisms/footer/Footer.js
+++ _source/organisms/footer/Footer.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, FormattedHTMLMessage, injectIntl } from 'react-intl';
@@ -45,17 +43,25 @@ class Footer extends Component {
           </section>
         ) }
         <section className={ classNames('footer__social', darkMode && 'footer__social--dark-mode') }>
-          <a className="footer__social-item" target="_blank" href="https://twitter.com/intent/tweet?text=booky.io%20%7C%20Online%20Bookmark%20manager.%20Your%20bookmarks%20always%20available.">
+          <a
+            className="footer__social-item"
+            target="_blank"
+            href="https://twitter.com/intent/tweet?text=booky.io%20%7C%20Online%20Bookmark%20manager.%20Your%20bookmarks%20always%20available."
+          >
             <Icon className="footer__icon" icon="twitter" color="light" />
             <label className="footer__label">{ 'Twitter' }</label>
           </a>
-          <a className="footer__social-item" target="_blank" href="https://plus.google.com/share?url=http%3A%2F%2Fbooky.io">
+          <a className="footer__social-item" target="_blank" href="https://plus.google.com/share?url=https%3A%2F%2Fbooky.io">
             <Icon className="footer__icon" icon="google-plus" color="light" />
             <label className="footer__label">{ 'Google+' }</label>
           </a>
-          <a className="footer__social-item" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fbooky.io">
+          <a className="footer__social-item" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fbooky.io">
             <Icon className="footer__icon" icon="facebook" color="light" />
             <label className="footer__label">{ 'Facebook' }</label>
+          </a>
+          <a className="footer__social-item" target="_blank" href="https://www.xing.com/spi/shares/new?sc_p=b7910_cb&url=https%3A%2F%2Fbooky.io">
+            <Icon className="footer__icon" icon="xing" color="light" />
+            <label className="footer__label">{ 'XING' }</label>
           </a>
         </section>
         <section className="footer__content">
@@ -76,10 +82,12 @@ class Footer extends Component {
               <FormattedMessage id="menu.legal" />
             </Link>
           </nav>
-          <LanguageSwitcher className="footer__language" />
-          <ButtonSmallLight className="footer__button" onClick={ () => window.scrollTo(0, 0) }>
-            <FormattedHTMLMessage id="footer.scroll" />
-          </ButtonSmallLight>
+          <div className="footer__wrapper">
+            <LanguageSwitcher className="footer__language" />
+            <ButtonSmallLight className="footer__button" onClick={ () => window.scrollTo(0, 0) }>
+              <FormattedHTMLMessage id="footer.scroll" />
+            </ButtonSmallLight>
+          </div>
           <Link to="/" title={ intl.formatMessage({ id: 'menu.home' }) } className="footer__logo">
             <img src="../../_assets/logo-primary.png" alt="Logo" height="36" />
           </Link>
