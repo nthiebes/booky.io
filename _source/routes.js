edd68148d6f87af0cc75f57736c4f4d97292316c
Nico Thiebes <nico.thiebes@xing.com>
2019-09-03 12:57:16 +0200

    use login screen as startpage

--- _source/routes.js
+++ _source/routes.js
@@ -4,7 +4,7 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
 import { restricted } from './templates/restricted';
-import Home from './pages/home';
+// import Home from './pages/home';
 import About from './pages/about';
 import Help from './pages/help';
 import Account from './pages/account';
@@ -35,13 +35,13 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={ Home } />
+        <Route exact path="/" component={ Login } />
         <Route path="/about" component={ About }/>
         <Route path="/help" component={ Help } />
         <Route path="/account" component={ restricted(Account) } />
         <Route path="/login" component={ Login } />
         <Route path="/join" component={ Join } />
-        <Route path="/feedback" component={ restricted(Feedback) } />
+        <Route path="/feedback" component={ Feedback } />
         <Route path="/privacy" component={ Privacy } />
         <Route path="/legal" component={ Legal } />
         <Route path="/contact" component={ Contact } />
