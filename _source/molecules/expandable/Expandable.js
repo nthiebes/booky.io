87ce6af07e4aec88ac0208351702bba104ef9a4c
Nico Thiebes <nico.thiebes@xing.com>
2019-09-06 22:56:34 +0200

    no class for expandable

--- _source/molecules/expandable/Expandable.js
+++ _source/molecules/expandable/Expandable.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import P from '../../atoms/paragraph';
 
-export default class Expandable extends Component {
-  render() {
-    const { className, headline, children } = this.props;
+const Expandable = ({ className, headline, children }) => (
+  <details className={ classNames('expandable', className) }>
+    <summary className="expandable__summary">
+      { headline }
+    </summary>
+    <P noPadding>
+      { children }
+    </P>
+  </details>
+);
 
-    return (
-      <details className={ classNames('expandable', className) }>
-        <summary className="expandable__summary">
-          { headline }
-        </summary>
-        <P noPadding>
-          { children }
-        </P>
-      </details>
-    );
-  }
-}
+export default Expandable;
 
 Expandable.propTypes = {
   className: PropTypes.string,
