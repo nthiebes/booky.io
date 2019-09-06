749aa296fda701282b7079f23eb58d7806e60fff
Nico Thiebes <nico.thiebes@xing.com>
2019-09-06 17:53:02 +0200

    update logo component

--- _source/atoms/logo/Logo.js
+++ _source/atoms/logo/Logo.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import './Logo.scss';
-
 export default class Logo extends Component {
   render() {
     const { className, src, alt, height, width, colorInverted } = this.props;
@@ -14,9 +12,9 @@ export default class Logo extends Component {
         alt={ alt } 
         height={ height } 
         width={ width } 
-        className={ classNames(colorInverted && 'logo--inverted', className && className )} 
+        className={ classNames(colorInverted && 'logo--inverted', className && className) } 
       />
-    )
+    );
   }
 }
 
@@ -27,11 +25,11 @@ Logo.propTypes = {
   colorInverted: PropTypes.bool,
   height: PropTypes.string,
   width: PropTypes.string 
-}
+};
 
 Logo.defaultProps = {
-  src: '../../_assets/logo-primary.png',
-  alt: 'Logo',
+  src: '../../_assets/logo.svg',
+  alt: '',
   height: '36',
-  colorInverted: false,
-}
+  colorInverted: false
+};
