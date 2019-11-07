8bbdebe80ad88984995f5f43ad35f333a57f3870
Nico Thiebes <nico.thiebes@xing.com>
2019-11-07 10:25:20 +0100

    split based on url

--- _source/routes.js
+++ _source/routes.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
 import { restricted } from './templates/restricted';
-import Home from './pages/home';
-import About from './pages/about';
-import Help from './pages/help';
-import Account from './pages/account';
-import Login from './pages/login';
-import Join from './pages/join';
-import Feedback from './pages/feedback';
-import Privacy from './pages/privacy';
-import Legal from './pages/legal';
-import Contact from './pages/contact';
-import Forgot from './pages/forgot';
-import NotFound from './pages/not-found';
+import Loading from './pages/loading';
+
+const Home = lazy(() => import('./pages/home'));
+const About = lazy(() => import('./pages/about'));
+const Help = lazy(() => import('./pages/help'));
+const Account = lazy(() => import('./pages/account'));
+const Login = lazy(() => import('./pages/login'));
+const Join = lazy(() => import('./pages/join'));
+const Feedback = lazy(() => import('./pages/feedback'));
+const Privacy = lazy(() => import('./pages/privacy'));
+const Legal = lazy(() => import('./pages/legal'));
+const Contact = lazy(() => import('./pages/contact'));
+const Forgot = lazy(() => import('./pages/forgot'));
+const NotFound = lazy(() => import('./pages/not-found'));
 
 class Routes extends Component {
   static propTypes = {
@@ -34,20 +36,22 @@ class Routes extends Component {
 
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About }/>
-        <Route path="/help" component={ Help } />
-        <Route path="/account" component={ restricted(Account) } />
-        <Route path="/login" component={ Login } />
-        <Route path="/join" component={ Join } />
-        <Route path="/feedback" component={ Feedback } />
-        <Route path="/privacy" component={ Privacy } />
-        <Route path="/legal" component={ Legal } />
-        <Route path="/contact" component={ Contact } />
-        <Route path="/forgot" component={ Forgot } />
-        <Route path="*" component={ NotFound } />
-      </Switch>
+      <Suspense fallback={ <Loading /> }>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About }/>
+          <Route path="/help" component={ Help } />
+          <Route path="/account" component={ restricted(Account) } />
+          <Route path="/login" component={ Login } />
+          <Route path="/join" component={ Join } />
+          <Route path="/feedback" component={ Feedback } />
+          <Route path="/privacy" component={ Privacy } />
+          <Route path="/legal" component={ Legal } />
+          <Route path="/contact" component={ Contact } />
+          <Route path="/forgot" component={ Forgot } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </Suspense>
     );
   }
 }
