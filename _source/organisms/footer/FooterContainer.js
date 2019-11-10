a67bf52456add75bd1a2f16ebaa72be183108de9
Nico Thiebes <nico.thiebes@xing.com>
2019-11-10 12:32:38 +0100

    use . or , for booky numbers

--- _source/organisms/footer/FooterContainer.js
+++ _source/organisms/footer/FooterContainer.js
@@ -1,15 +1,14 @@
 import { connect } from 'react-redux';
 import Component from './Footer';
 
-export const mapStateToProps = function(state) {
-  return {
-    dashboardsOpen: state.user.settings.pinned,
-    hasSidebar: state.user.settings.dashboardsStyle === 'sidebar',
-    loggedIn: state.user.loggedIn,
-    toolbarSticky: state.user.settings.stickyToolbar,
-    darkMode: state.user.settings.darkMode
-  };
-};
+export const mapStateToProps = (state) => ({
+  dashboardsOpen: state.user.settings.pinned,
+  hasSidebar: state.user.settings.dashboardsStyle === 'sidebar',
+  loggedIn: state.user.loggedIn,
+  toolbarSticky: state.user.settings.stickyToolbar,
+  darkMode: state.user.settings.darkMode,
+  locale: state.intl.locale
+});
 
 const Container = connect(
   mapStateToProps
