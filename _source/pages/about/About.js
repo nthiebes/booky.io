539e65ef7685975caa2689189fd704398df57925
Nico Thiebes <nico.thiebes@xing.com>
2020-02-12 22:31:46 +0100

    add changelog to about page

--- _source/pages/about/About.js
+++ _source/pages/about/About.js
@@ -1,20 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { FormattedMessage } from 'react-intl';
+import { format } from 'date-fns';
 
 import Page from '../../templates/page';
-// import { H1, H2 } from '../../atoms/headline';
-// import P from '../../atoms/paragraph';
+import { H1, H2, H3 } from '../../atoms/headline';
+import P from '../../atoms/paragraph';
 import Section from '../../molecules/section';
-import Empty from '../../molecules/empty';
+import { List, ListItem } from '../../atoms/list';
 
 export default class About extends Component {
+  state = {
+    releases: []
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/repos/nthiebes/booky.io/releases')
+      .then((response) => response.json())
+      .then((releases) => {
+        this.setState({ releases });
+      })
+      .catch();
+  }
+
   render() {
+    const { releases } = this.state;
+
     return (
       <Page>
         <Section>
-          <Empty illustration="monitor-window">
+          <H1>
+            <FormattedMessage id="about.title" />
+          </H1>
+          <P first>
             <FormattedMessage id="misc.comingSoon" />
-          </Empty>
+          </P>
+          <H2>
+            <FormattedMessage id="about.betaUpdates" />
+          </H2>
+          { /* eslint-disable-next-line camelcase */ }
+          { releases.map(({ id, name, body, published_at }) => {
+            const lines = body.split('\n');
+            
+            return (
+              <Fragment key={ id }>
+                <H3 style="h4">
+                  <span>{ `${name} -` }</span>
+                  <time className="about__date">{ format(new Date(published_at), 'MM/dd/yyyy') }</time>
+                </H3>
+                <List>
+                  { lines.map((line, index) => (
+                    <ListItem key={ index }>
+                      { line.replace(/- /g, '') }
+                      { index < lines.length - 1 && <br /> }
+                    </ListItem>
+                  )) }
+                </List>
+              </Fragment>
+            );
+          }) }
         </Section>
       </Page>
     );
