9c0c046ad94c6f90af4476437f946dbf2e4bc0f4
Nico Thiebes <nico.thiebes@xing.com>
2020-02-22 12:16:39 +0100

    fix linter

--- _source/molecules/expandable/Expandable.js
+++ _source/molecules/expandable/Expandable.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import P from '../../atoms/paragraph';
-
 const Expandable = ({ className, headline, children, darkMode }) => (
   <details className={ classNames('expandable', className) }>
     <summary className={ classNames('expandable__summary', darkMode && 'expandable__summary--dark-mode') }>
