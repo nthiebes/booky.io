cb6c1e189010cfcf5eece3dee0b40af1ce701c48
Nico Thiebes <nico.thiebes@xing.com>
2020-05-19 18:57:59 +0200

    rework success message and add hidden website form field

--- _source/atoms/messages/success/SuccessMessage.js
+++ _source/atoms/messages/success/SuccessMessage.js
@@ -7,45 +7,51 @@ import P from '../../paragraph';
 import Icon from '../../icon';
 
 export default class SuccessMessage extends Component {
-  constructor(props) {
-    super(props);
+  static propTypes = {
+    message: PropTypes.string,
+    className: PropTypes.string,
+    hasIcon: PropTypes.bool,
+    icon: PropTypes.string
+  }
+  
+  static defaultProps = {
+    message: 'success.default',
+    icon: 'check'
+  }
 
-    this.animate = this.animate.bind(this);
-    this.state = {
-      animate: false
-    };
+  state = {
+    animate: false
   }
 
   componentDidMount() {
     window.setTimeout(this.animate, 100);
   }
 
-  animate() {
+  animate = () => {
     this.setState({
       animate: true
     });
   }
 
   render() {
-    const { message, className, hasIcon } = this.props;
+    const { message, className, hasIcon, icon } = this.props;
     const { animate } = this.state;
 
     return (
-      <P className={ classNames('success', animate && 'success--animate', className && className) } role="alert">
-        { hasIcon && <Icon icon="check" color="green" ignoreDarkMode className="success__icon" /> }
-        <FormattedMessage id={ message } />
+      <P className={ classNames('success', className) } role="alert">
+        { hasIcon && (
+          <Icon
+            icon={ icon }
+            color="green"
+            size="large"
+            ignoreDarkMode
+            className={ classNames('success__icon', animate && 'success__icon--animate') }
+          />
+        ) }
+        <span className={ classNames('success__text', animate && 'success__text--animate') }>
+          <FormattedMessage id={ message } />
+        </span>
       </P>
     );
   }
 }
-
-SuccessMessage.propTypes = {
-  message: PropTypes.string,
-  className: PropTypes.string,
-  hasIcon: PropTypes.bool
-};
-
-
-SuccessMessage.defaultProps = {
-  message: 'success.default'
-};
--- _source/pages/join/Join.js
+++ _source/pages/join/Join.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, FormattedHTMLMessage, injectIntl } from 'react-intl';
-import { withRouter } from 'react-router-dom';
 
 import Page from '../../templates/page';
 import { H1 } from '../../atoms/headline';
 import P from '../../atoms/paragraph';
 import Link from '../../atoms/link';
 import Input from '../../atoms/input';
-import { ErrorMessage } from '../../atoms/messages';
+import { ErrorMessage, SuccessMessage } from '../../atoms/messages';
 import { ButtonLargeBlue } from '../../atoms/button';
 import Checkbox from '../../atoms/checkbox';
 import Form from '../../molecules/form';
@@ -20,7 +19,6 @@ import './Join.scss';
 class Join extends Component {
   static propTypes = {
     intl: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
     join: PropTypes.func.isRequired,
     language: PropTypes.string.isRequired
   }
@@ -48,23 +46,31 @@ class Join extends Component {
   }
 
   handleSubmit = (params) => {
-    const { history, join } = this.props;
+    const { join } = this.props;
 
     this.setState({
       pending: true,
       error: false
     });
 
-    console.log(params);
-
     join({
       params,
       onSuccess: (data) => {
         console.log('success', data);
-        // history.push('/');
+
+        window.scrollTo(0, 0);
+        this.setState({
+          pending: false,
+          success: true
+        });
       },
       onError: (error) => {
         console.log('error', error);
+
+        // this.setState({
+        //   pending: false,
+        //   success: true
+        // });
         this.setState({
           pending: false,
           error
@@ -75,91 +81,103 @@ class Join extends Component {
 
   render() {
     const { intl, language } = this.props;
-    const { username, email, password, pending, showPassword, error } = this.state;
+    const { username, email, password, pending, showPassword, error, success } = this.state;
 
     return (
       <Page>
         <Section className="join">
-          <Form onSubmit={ this.handleSubmit } className="join__form">
-            <H1>
-              <FormattedMessage id="join.headline" />
-            </H1>
-            <Input
-              value={ username }
-              name="username"
-              id="username"
-              autoComplete="username"
-              label={ intl.formatMessage({ id: 'login.username' }) }
-              onChange={ this.handleInputChange }
-              maxLength="50"
-              required
-              disabled={ pending }
-            />
-            <Input
-              value={ email }
-              name="email"
-              id="email"
-              autoComplete="email"
-              label={ intl.formatMessage({ id: 'login.email' }) }
-              onChange={ this.handleInputChange }
-              maxLength="150"
-              required
-              type="email"
-              requirements={ intl.formatMessage({ id: 'misc.validEmail' }) }
-              disabled={ pending }
-            />
-            <Input
-              value={ password }
-              name="password"
-              id="password"
-              autoComplete="current-password"
-              label={ intl.formatMessage({ id: 'login.password' }) }
-              onChange={ this.handleInputChange }
-              maxLength="225"
-              required
-              type={ showPassword ? 'text' : 'password' }
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-              requirements={ intl.formatMessage({ id: 'misc.validPassword' }) }
-              disabled={ pending }
-            />
-            <Input
-              value={ language }
-              name="language"
-              type="hidden"
-            />
-            <Checkbox
-              label={ intl.formatMessage({ id: 'login.showPassword'}) }
-              id="show-password"
-              onChange={ this.handleCheckboxChange }
-            />
-            <ButtonLargeBlue
-              icon="join"
-              type="submit"
-              pending={ pending }
-              disabled={ pending }
-              contentBefore
-            >
-              <FormattedHTMLMessage id="join.joinNow" />
-            </ButtonLargeBlue>
-            { error && <ErrorMessage hasIcon /> }
-            <P className="join__login">
-              <FormattedMessage id="join.registered" />
-              { ' ' }
-              <Link to="/login">
-                <FormattedMessage id="join.login" />
-              </Link>
-            </P>
-          </Form>
-          <Illustration
-            name="monitor-window"
-            width="300"
-            height="300"
-            className="join__illustration"
-          />
+          { success ? (
+            <SuccessMessage message="join.success" hasIcon icon="smile" />
+          ) : (
+            <Fragment>
+              <Form onSubmit={ this.handleSubmit } className="join__form">
+                <H1>
+                  <FormattedMessage id="join.headline" />
+                </H1>
+                <Input
+                  value={ username }
+                  name="username"
+                  id="username"
+                  autoComplete="username"
+                  label={ intl.formatMessage({ id: 'login.username' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="50"
+                  required
+                  disabled={ pending }
+                />
+                <Input
+                  value={ email }
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  label={ intl.formatMessage({ id: 'login.email' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="150"
+                  required
+                  type="email"
+                  requirements={ intl.formatMessage({ id: 'misc.validEmail' }) }
+                  disabled={ pending }
+                />
+                <Input
+                  value={ password }
+                  name="password"
+                  id="password"
+                  autoComplete="current-password"
+                  label={ intl.formatMessage({ id: 'login.password' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="225"
+                  required
+                  type={ showPassword ? 'text' : 'password' }
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+                  requirements={ intl.formatMessage({ id: 'misc.validPassword' }) }
+                  disabled={ pending }
+                />
+                <input
+                  value={ language }
+                  name="language"
+                  type="hidden"
+                />
+                <input
+                  className="join__website"
+                  name="website"
+                  autoComplete="off"
+                  tabIndex="-1"
+                />
+                <Checkbox
+                  label={ intl.formatMessage({ id: 'login.showPassword'}) }
+                  id="show-password"
+                  onChange={ this.handleCheckboxChange }
+                />
+                <ButtonLargeBlue
+                  icon="join"
+                  type="submit"
+                  pending={ pending }
+                  disabled={ pending }
+                  contentBefore
+                >
+                  <FormattedHTMLMessage id="join.joinNow" />
+                </ButtonLargeBlue>
+                { error && <ErrorMessage hasIcon /> }
+                <P className="join__login">
+                  <FormattedMessage id="join.registered" />
+                  { ' ' }
+                  <Link to="/login">
+                    <FormattedMessage id="join.login" />
+                  </Link>
+                </P>
+              </Form>
+              <Illustration
+                name="monitor-window"
+                width="300"
+                height="300"
+                className="join__illustration"
+              />
+            </Fragment>
+          )}
         </Section>
       </Page>
     );
   }
 }
 
-export default injectIntl(withRouter(Join));
+export default injectIntl(Join);
