04032746095e44db4ae29931023f4c4b39a877e0
Nico Thiebes <nico.thiebes@xing.com>
2020-07-14 21:19:42 +0200

    update password forgotten page

--- _source/pages/forgot/Forgot.js
+++ _source/pages/forgot/Forgot.js
@@ -1,27 +1,128 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage, FormattedHTMLMessage, injectIntl } from 'react-intl';
 
 import Page from '../../templates/page';
-// import { H1 } from '../../atoms/headline';
+import { H1 } from '../../atoms/headline';
+import Input from '../../atoms/input';
+import { ErrorMessage, SuccessMessage } from '../../atoms/messages';
+import { ButtonLargeBlue } from '../../atoms/button';
+import Form from '../../molecules/form';
 import Section from '../../molecules/section';
-import Empty from '../../molecules/empty';
+import Illustration from '../../atoms/illustration';
 
-export default class Forgot extends Component {
+class Forgot extends Component {
+  static propTypes = {
+    intl: PropTypes.object.isRequired,
+    forgot: PropTypes.func.isRequired
+  }
+
+  state = {
+    username: '',
+    email: '',
+    pending: false,
+    error: null
+  }
+
+  handleInputChange = (value, name) => {
+    this.setState({
+      [name]: value,
+      pending: false
+    });
+  }
+
+  handleSubmit = (params) => {
+    const { forgot } = this.props;
+
+    this.setState({
+      pending: true,
+      error: false
+    });
+
+    forgot({
+      params,
+      onSuccess: () => {
+        // console.log('success', data);
+
+        window.scrollTo(0, 0);
+        this.setState({
+          pending: false,
+          success: true
+        });
+      },
+      onError: (error) => {
+        // console.log('error', error);
+
+        this.setState({
+          pending: false,
+          error
+        });
+      }
+    });
+  }
+
   render() {
+    const { intl } = this.props;
+    const { username, email, pending, error, success } = this.state;
+
     return (
       <Page>
-        <Section>
-          <Empty illustration="monitor-window">
-            <FormattedMessage id="misc.comingSoon" />
-          </Empty>
+        <Section className="forgot">
+          { success ? (
+            <SuccessMessage message="forgot.success" hasIcon icon="smile" />
+          ) : (
+            <Fragment>
+              <Form onSubmit={ this.handleSubmit } className="forgot__form">
+                <H1>
+                  <FormattedMessage id="forgot.headline" />
+                </H1>
+                <Input
+                  value={ username }
+                  name="username"
+                  id="username"
+                  autoComplete="username"
+                  label={ intl.formatMessage({ id: 'login.username' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="50"
+                  required
+                  disabled={ pending }
+                />
+                <Input
+                  value={ email }
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  label={ intl.formatMessage({ id: 'login.email' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="150"
+                  required
+                  type="email"
+                  requirements={ intl.formatMessage({ id: 'misc.validEmail' }) }
+                  disabled={ pending }
+                />
+                <ButtonLargeBlue
+                  icon="send"
+                  type="submit"
+                  pending={ pending }
+                  disabled={ pending }
+                  contentBefore
+                >
+                  <FormattedHTMLMessage id="forgot.button" />
+                </ButtonLargeBlue>
+                { error && <ErrorMessage message={ error } hasIcon /> }
+              </Form>
+              <Illustration
+                name="postman-receive-letter"
+                width="300"
+                height="300"
+                className="forgot__illustration"
+              />
+            </Fragment>
+          )}
         </Section>
       </Page>
     );
   }
 }
 
-/*
-<H1>
-  <FormattedMessage id="forgot.title" />
-</H1>
- */
+export default injectIntl(Forgot);
