ae8d83e8975c62eda43338fb85fc91151c820d8f
Nico Thiebes <nico.thiebes@gmail.com>
2020-09-23 21:07:08 +0200

    import page wip

--- _source/pages/account/tabs/AccountImport.js
+++ _source/pages/account/tabs/AccountImport.js
@@ -1,45 +1,110 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, injectIntl } from 'react-intl';
+import Dropzone from 'react-dropzone';
 
-// import { ButtonLargeBlue } from '../../../atoms/button';
-// import FileUpload from '../../../atoms/file-upload';
-import Empty from '../../../molecules/empty';
+import { ButtonLargeBlue } from '../../../atoms/button';
+// import P from '../../../atoms/paragraph';
+import Checkbox from '../../../atoms/checkbox';
+import { ErrorMessage, SuccessIllustration } from '../../../atoms/messages';
+import Form from '../../../molecules/form';
 
-class AccountImport extends Component {
+class AccountImport extends PureComponent {
+  static propTypes = {
+    intl: PropTypes.object.isRequired
+  }
+
+  state = {
+    files: [],
+    error: null,
+    success: false,
+    pending: false
+  }
+
+  handleDrop = (files) => {
+    console.log(files);
+    this.setState({files});
+  }
+
+  handleSubmit = (data) => {
+    console.log(data);
+    // const { updateUser } = this.props;
+
+    // this.setState({
+    //   pending: true,
+    //   error: false,
+    //   success: false
+    // });
+  }
+
   render() {
-    // const { intl } = this.props;
-
+    const { intl } = this.props;
+    const { error, success, pending } = this.state;
+    // const onDrop = useCallback((acceptedFiles) => {
+    //   console.log(acceptedFiles);
+    // }, []);
+    // const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+    // const [success, setSuccess] = useState(false);
+    // const [error, setError] = useState(null);
+    // const [pending, setPending] = useState(false);
+    
+  
+    if (success) {
+      <SuccessIllustration message="resend.success" illustration="join-success" width="400" />;
+    }
+  
     return (
-      <Empty illustration="empty">
-        <FormattedMessage id="misc.comingSoon" />
-      </Empty>
+      <>
+        <Checkbox
+          label={ intl.formatMessage({ id: 'account.exportFavicons' }) }
+          id="show-password"
+          // onChange={ this.handleCheckboxChange }
+          checked={ true }
+        />
+        <ol className="account__import-steps">
+          <li className="account__import-step">
+            <FormattedMessage id="account.importStep1" values={ { strong: (msg) => <strong>{msg}</strong> } } />
+          </li>
+          <li className="account__import-step">
+            <FormattedMessage id="account.importStep2" />
+          </li>
+          <li className="account__import-step">
+            <FormattedMessage id="account.importStep3" />
+          </li>
+        </ol>
+        <Form className="account__form" onSubmit={ this.handleSubmit }>
+          <Dropzone onDrop={ this.handleDrop }>
+            {({getRootProps, getInputProps, isDragActive}) => (
+              <section>
+                <div { ...getRootProps({
+                  'aria-label': 'dies das label'
+                }) }>
+                  <input { ...getInputProps({
+                    name: 'bookmarks'
+                  }) } />
+                  <div>{
+                    isDragActive
+                      ? <p>Drop the files here ...</p>
+                      : <p>Drag 'n' drop some files here, or click to select files</p>
+                  }</div>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+          <ButtonLargeBlue
+            icon="upload"
+            type="submit"
+            pending={ pending }
+            disabled={ pending }
+            contentBefore
+          >
+            <FormattedMessage id="account.import" />
+          </ButtonLargeBlue>
+          { error && <ErrorMessage message={ error } hasIcon /> }
+        </Form>
+      </>
     );
   }
 }
 
-AccountImport.propTypes = {
-  intl: PropTypes.object.isRequired
-};
-
 export default injectIntl(AccountImport);
-
-/*
-<Fragment>
-  <ol className="account__import-steps">
-    <li className="account__import-step">
-      <FormattedMessage id="account.importStep1" values={ { b: (msg) => <b>{msg}</b> } } />
-    </li>
-    <li className="account__import-step">
-      <FormattedMessage id="account.importStep2" />
-      <FileUpload name="file" />
-    </li>
-    <li className="account__import-step">
-      <FormattedMessage id="account.importStep3" />
-    </li>
-  </ol>
-  <ButtonLargeBlue icon="upload" contentBefore disabled>
-    <FormattedMessage id="account.import" />
-  </ButtonLargeBlue>
-</Fragment>
-*/
