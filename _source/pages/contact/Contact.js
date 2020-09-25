b63a35151e008420a8f867cdbf7fa61d4a58f6a0
Nico Thiebes <nico.thiebes@gmail.com>
2020-09-25 10:26:52 +0200

    add contact form

--- _source/pages/contact/Contact.js
+++ _source/pages/contact/Contact.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, injectIntl } from 'react-intl';
 
 import fetcher from '../../_utils/fetcher';
 import Page from '../../templates/page';
-// import { H1 } from '../../atoms/headline';
-// import P from '../../atoms/paragraph';
-// import Input from '../../atoms/input';
-// import { ErrorMessage, SuccessIllustration } from '../../atoms/messages';
-// import { ButtonLargeBlue } from '../../atoms/button';
-// import Textarea from '../../atoms/textarea';
-// import Form from '../../molecules/form';
+import { H1 } from '../../atoms/headline';
+import P from '../../atoms/paragraph';
+import Input from '../../atoms/input';
+import { ErrorMessage, SuccessIllustration } from '../../atoms/messages';
+import { ButtonLargeBlue } from '../../atoms/button';
+import Textarea from '../../atoms/textarea';
+import Illustration from '../../atoms/illustration';
+import Form from '../../molecules/form';
 import Section from '../../molecules/section';
-import Empty from '../../molecules/empty';
 
-class Contact extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      username: props.username,
-      email: props.email,
-      message: '',
-      pending: false,
-      error: null,
-      success: false
-    };
+class Contact extends PureComponent {
+  static propTypes = {
+    intl: PropTypes.object.isRequired,
+    username: PropTypes.string,
+    email: PropTypes.string
   }
 
-  handleInputChange(value, name) {
+  state = {
+    name: this.props.username,
+    email: this.props.email,
+    message: '',
+    pending: false,
+    error: null,
+    success: false
+  }
+
+  handleInputChange = (value, name) => {
     this.setState({
       [name]: value,
-      success: false,
       pending: false
     });
   }
 
-  handleSubmit(params) {
+  handleSubmit = (params) => {
     this.setState({
       pending: true,
       error: false
@@ -48,104 +47,96 @@ class Contact extends Component {
       url: '/contact',
       method: 'POST',
       params,
-      onSuccess: (data) => {
-        // console.log('success:', data);
-
-        window.setTimeout(() => {
-          this.setState({
-            pending: false,
-            error: data.error,
-            success: true
-          });
-        }, 300);
+      onSuccess: () => {
+        this.setState({
+          pending: false,
+          success: true
+        });
       },
-      onError: () => {
-        window.setTimeout(() => {
-          this.setState({
-            pending: false,
-            error: 'error.default'
-          });
-        }, 300);
+      onError: (error) => {
+        this.setState({
+          pending: false,
+          success: true
+        });
       }
     });
   }
 
   render() {
-    // const { intl } = this.props;
-    // const { username, email, message, pending, error, success } = this.state;
+    const { intl } = this.props;
+    const { name, email, message, pending, error, success } = this.state;
 
     return (
       <Page>
-        <Section>
-          <Empty illustration="empty">
-            <FormattedMessage id="misc.comingSoon" />
-          </Empty>
+        <Section className="contact">
+          { success ? (
+            <SuccessIllustration message="contact.success" illustration="join-success" width="400" />
+          ) : (
+            <>
+              <Form onSubmit={ this.handleSubmit }>
+                <H1>
+                  <FormattedMessage id="contact.headline" />
+                </H1>
+                <P first>
+                  <FormattedMessage id="contact.text" />
+                </P>
+                <Input
+                  value={ name }
+                  name="name"
+                  id="name"
+                  autoComplete="username"
+                  label={ intl.formatMessage({ id: 'misc.name' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="50"
+                  required
+                  disabled={ pending }
+                />
+                <Input
+                  value={ email }
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  label={ intl.formatMessage({ id: 'login.email' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="150"
+                  required
+                  type="email"
+                  requirements={ intl.formatMessage({ id: 'misc.validEmail' }) }
+                  disabled={ pending }
+                />
+                <Textarea
+                  value={ message }
+                  name="message"
+                  id="message"
+                  label={ intl.formatMessage({ id: 'contact.message' }) }
+                  onChange={ this.handleInputChange }
+                  maxLength="5000"
+                  required
+                  disabled={ pending }
+                  rows={ 5 }
+                  className="contact__message"
+                />
+                <ButtonLargeBlue
+                  icon="send"
+                  type="submit"
+                  pending={ pending }
+                  disabled={ pending }
+                  contentBefore
+                >
+                  <FormattedMessage id="button.send" values={ { b: (msg) => <b>{msg}</b> } } />
+                </ButtonLargeBlue>
+                { error && <ErrorMessage message={ error } hasIcon /> }
+              </Form>
+              <Illustration
+                name="email"
+                className="contact__illustration booky--hide-mobile-tablet"
+              />
+            </>
+          ) }
         </Section>
-        {/* <Section compact>
-          <Form onSubmit={ this.handleSubmit }>
-            <H1>
-              <FormattedMessage id="contact.headline" />
-            </H1>
-            <P first>
-              <FormattedMessage id="contact.text" />
-            </P>
-            <Input
-              value={ username }
-              name="username"
-              id="username"
-              autoComplete="username"
-              label={ intl.formatMessage({ id: 'login.username' }) }
-              onChange={ this.handleInputChange }
-              maxLength="50"
-              required
-              disabled={ pending }
-            />
-            <Input
-              value={ email }
-              name="email"
-              id="email"
-              autoComplete="email"
-              label={ intl.formatMessage({ id: 'login.email' }) }
-              onChange={ this.handleInputChange }
-              maxLength="150"
-              required
-              type="email"
-              requirements={ intl.formatMessage({ id: 'misc.validEmail' }) }
-              disabled={ pending }
-            />
-            <Textarea
-              value={ message }
-              name="message"
-              id="message"
-              label={ intl.formatMessage({ id: 'contact.message' }) }
-              onChange={ this.handleInputChange }
-              maxLength="5000"
-              required
-              disabled={ pending }
-              rows={ 5 }
-            />
-            { error && <ErrorMessage message={ error } hasIcon /> }
-            { success && <SuccessIllustration message="contact.success" hasIcon /> }
-            <ButtonLargeBlue
-              icon="message"
-              type="submit"
-              pending={ pending }
-              disabled={ pending }
-              contentBefore
-            >
-              <FormattedMessage id="button.send" values={ { b: (msg) => <b>{msg}</b> } } />
-            </ButtonLargeBlue>
-          </Form>
-        </Section> */}
       </Page>
     );
   }
 }
-
-Contact.propTypes = {
-  intl: PropTypes.object.isRequired,
-  username: PropTypes.string,
-  email: PropTypes.string
-};
 
 export default injectIntl(Contact);
