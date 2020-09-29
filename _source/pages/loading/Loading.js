0ecf8e1d388d91e6d76b662e5aa724d396fc3171
Nico Thiebes <nico.thiebes@gmail.com>
2020-09-29 16:40:15 +0200

    fix loading behaviour

--- _source/pages/loading/Loading.js
+++ _source/pages/loading/Loading.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Page from '../../templates/page';
+import Extension from '../../templates/extension';
 import Section from '../../molecules/section';
 import Icon from '../../atoms/icon';
 
 export default class Loading extends Component {
+  static propTypes = {
+    isExtension: PropTypes.bool.isRequired
+  }
+
   render() {
+    const TemplateComponent = this.props.isExtension ? Extension : Page;
+
     return (
-      <Page>
+      <TemplateComponent>
         <Section>
           <Icon icon="spinner" className="loading-page__spinner" />
         </Section>
-      </Page>
+      </TemplateComponent>
     );
   }
 }
