bc9164214d03c27bba0f5c69381c3d05ce9e9301
Nico Thiebes <nico.thiebes@gmail.com>
2020-10-01 09:53:36 +0200

    add browser detection

--- _source/config.js
+++ _source/config.js
@@ -1,13 +1,27 @@
+/* eslint-disable no-nested-ternary */
+const isOpera =
+  // eslint-disable-next-line no-undef
+  (Boolean(window.opr) && Boolean(opr.addons)) ||
+  Boolean(window.opera) ||
+  navigator.userAgent.indexOf(' OPR/') >= 0;
+const isFirefox = typeof InstallTrigger !== 'undefined';
+const isChrome = Boolean(window.chrome) && (Boolean(window.chrome.webstore) || Boolean(window.chrome.runtime));
+const isEdge = isChrome && navigator.userAgent.indexOf('Edg') !== -1;
+const isDevEnvironment = process.env.NODE_ENV === 'development';
+const browser = isChrome ? 'chrome'
+  : isOpera ? 'opera'
+    : isFirefox ? 'firefox'
+      : isEdge ? 'edge'
+        : 'chrome';
 const extensionIds = {
   chromeDev: 'chrome-extension://cdgbikmincdhncjonjcldflnkdbmbgco',
   chromeProd: 'chrome-extension://pmcpkkipiedakcaolhnbijibndfemckf',
-  firefox: 'moz-extension://ffe1d848-0e95-e247-8323-c93f1cc19443',
-  opera: 'op-extension://cdgbikmincdhncjonjcldflnkdbmbgco'
+  firefoxDev: 'moz-extension://ffe1d848-0e95-e247-8323-c93f1cc19443',
+  firefoxProd: 'moz-extension://ffe1d848-0e95-e247-8323-c93f1cc19443',
+  operaDev: 'op-extension://cdgbikmincdhncjonjcldflnkdbmbgco',
+  operaProd: 'op-extension://cdgbikmincdhncjonjcldflnkdbmbgco'
 };
-// const browser = '';
 
 export const config = {
-  extensionId: process.env.NODE_ENV === 'development' ? extensionIds.chromeDev : extensionIds.chromeProd
-  // extensionId: extensionIds.firefox
-  // extensionId: extensionIds.opera
+  extensionId: isDevEnvironment ? extensionIds[`${browser}Dev`] : extensionIds[`${browser}Prod`]
 };
