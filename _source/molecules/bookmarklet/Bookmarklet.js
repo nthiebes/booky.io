d2b27c94d8dc6b6576a2d4c3e1a61782009e7896
Nico Thiebes <nico.thiebes@gmail.com>
2020-10-04 01:14:22 +0200

    update bookmarklet

--- _source/molecules/bookmarklet/Bookmarklet.js
+++ _source/molecules/bookmarklet/Bookmarklet.js
@@ -1,14 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, injectIntl } from 'react-intl';
 
 import { ErrorMessage } from '../../atoms/messages';
+import Icon from '../../atoms/icon';
 import javascript from './javascript';
-
-const updatedJavascript = process.env.NODE_ENV === 'production'
-  ? javascript.replace(/http:\/\/localhost:3000/, 'https://beta.booky.io')
-  : javascript;
 
 class Bookmarklet extends PureComponent {
   static propTypes = {
@@ -21,6 +20,7 @@ class Bookmarklet extends PureComponent {
 
   handleBookmarkletClick = (event) => {
     event.preventDefault();
+
     this.setState({
       hasClicked: true
     });
@@ -33,11 +33,23 @@ class Bookmarklet extends PureComponent {
     return (
       <Fragment>
         <a
-          className="bookmarklet button button--small button--small-primary button--solid"
-          href={ `javascript:${updatedJavascript}` }
+          className="bookmarklet button button--large button--large-primary button--solid"
           onClick={ this.handleBookmarkletClick }
+          ref={ (node) => {
+            if (node) {
+              node.setAttribute('href', `javascript:${javascript}`);
+            }
+          } }
         >
-          <FormattedMessage id="bookmarklet.text" values={ { b: (msg) => <b>{msg}</b> } } />
+          <Icon
+            icon="star"
+            color="light"
+            className="button__icon"
+            ignoreDarkMode
+          />
+          <span className="button__text">
+            <FormattedMessage id="bookmarklet.text" values={ { b: (msg) => <b>{msg}</b> } } />
+          </span>
         </a>
         { hasClicked && (
           <ErrorMessage
