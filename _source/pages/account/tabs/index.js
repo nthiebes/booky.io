5d183b6b9821c9c6e24bc2d0ccf3ccd1ee317750
Nico Thiebes <nico.thiebes@gmail.com>
2020-10-14 19:49:12 +0200

    update account manage page

--- _source/pages/account/tabs/index.js
+++ _source/pages/account/tabs/index.js
@@ -2,12 +2,10 @@ import AccountData from './AccountDataContainer';
 import AccountImport from './AccountImport';
 import AccountExport from './AccountExport';
 import AccountManage from './AccountManageContainer';
-import AccountStatistics from './AccountStatistics';
 
 export {
   AccountData,
   AccountImport,
   AccountExport,
-  AccountManage,
-  AccountStatistics
+  AccountManage
 };
