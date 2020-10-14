c6d30fffeea54a35be1fafc1959534484014fcfd
Nico Thiebes <nico.thiebes@gmail.com>
2020-10-14 15:24:36 +0200

    remember extension language

--- _source/_utils/language/index.js
+++ _source/_utils/language/index.js
@@ -2,11 +2,15 @@ import * as Cookies from 'es-cookie';
 
 export const setLanguage = (language) => {
   const cookieConsent = Cookies.get('cookieConsent');
+  const isExtension = Boolean(window.location.pathname.match(/extension/gi));
 
   if (cookieConsent === 'true') {
     Cookies.set('lang', language, { expires: 18250,
       secure: process.env.NODE_ENV !== 'development'
     });
+  }
+  if (isExtension) {
+    localStorage.setItem('lang', language);
   }
   document.documentElement.setAttribute('lang', language);
 };
