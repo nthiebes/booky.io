dc6cbfa1719225d18783fba29c6a55b98f33ecf2
Nico Thiebes <nico.thiebes@gmail.com>
2020-10-17 15:36:46 +0200

    feedback link only in beta

--- _source/organisms/sidebar/SidebarContainer.js
+++ _source/organisms/sidebar/SidebarContainer.js
@@ -8,7 +8,8 @@ export const mapStateToProps = (state) => ({
   open: state.sidebar.open,
   hasSidebar: state.user.settings.dashboardsStyle === 'sidebar',
   darkMode: state.user.settings.darkMode,
-  color: state.user.settings.navigationBarColor
+  color: state.user.settings.navigationBarColor,
+  isBeta: state.user.isBeta
 });
 
 export const mapDispatchToProps = {
