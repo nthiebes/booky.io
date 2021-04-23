8adc04505398f50e4afeed50d6d9c8db39896e42
Nico Thiebes <nico.thiebes@gmail.com>
2021-04-23 17:35:48 +0200

    update version and hide survey banner

--- _source/pages/home/Home.js
+++ _source/pages/home/Home.js
@@ -55,7 +55,8 @@ class Home extends Component {
   };
 
   state = {
-    showSurveyBanner: !localStorage.getItem('hideSurveyBanner')
+    // showSurveyBanner: !localStorage.getItem('hideSurveyBanner')
+    showSurveyBanner: false
   };
 
   componentDidMount() {
