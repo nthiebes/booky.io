176d5eaa368e0af7d747bce7d228d2bad994a89a
Nico Thiebes <nico.thiebes@gmail.com>
2021-06-05 00:54:42 +0200

    fix linter and command

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -19,4 +19,4 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
