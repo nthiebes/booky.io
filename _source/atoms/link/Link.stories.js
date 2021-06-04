92c20708584c7152db8f1e516dc25745d0c16f67
Nico Thiebes <nico.thiebes@gmail.com>
2021-06-04 23:32:38 +0200

    remove storybook

--- _source/atoms/link/Link.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Link from './index';
-
-import '../../templates/booky/Booky.scss';
-
-storiesOf('Link', module).add('with text', () => (
-  <Link href="/booky">{'Awesome link'}</Link>
-));
